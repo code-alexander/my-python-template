a4737028bbf8ab5ec25a6982d6098ff1a40957f3
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-01-17 15:19:46 +1000

    test: adding more file tests

---
+++ tests/test_template.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import copier
+
+
+def test_folders(delete_temp_dir):
+    copier.run_copy(
+        src_path="src/my_python_template/",
+        dst_path="tests/temp/",
+        data={
+            "project_name": "Test Project",
+            "distribution_name": "test-project",
+            "import_name": "test_project",
+            "project_description": "`Test Project` is a Python package 🚀",
+        },
+    )
+    assert Path("tests/temp/src/test_project/__init__.py").is_file(), (
+        "`src/test_project/__init__.py` file not found"
+    )
+    assert Path("tests/temp/tests").is_dir(), "`tests` folder not found"
+    assert Path("tests/temp/.github/workflows/ci.yaml").is_file(), (
+        "`.github/workflows/ci.yaml` file not found"
+    )
+    assert Path("tests/temp/.pre-commit-config.yaml").is_file(), (
+        "`.pre-commit-config.yaml` file not found"
+    )
+    assert Path("tests/temp/README.md").is_file(), "`README.md` file not found"
+    assert Path("tests/temp/pyproject.toml").is_file(), (
+        "`pyproject.toml` file not found"
+    )
