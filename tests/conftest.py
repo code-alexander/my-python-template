5e6817caa52b4d151b0168e53fc1d87da6a2e517
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-01-18 12:32:16 +1000

    chore: adding ruff config for pre-commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
+"""pytest fixtures for `my-python-template`."""
+
 import shutil
+from collections.abc import Generator
 
 import pytest
 
 
 @pytest.fixture
-def delete_temp_dir():
-    print("Deleting `tests/temp/`")
-    shutil.rmtree("tests/temp/", ignore_errors=True)
-    yield
-    print("Deleting `tests/temp/`")
-    shutil.rmtree("tests/temp/", ignore_errors=True)
+def delete_temp_dir() -> Generator[None, None, None]:
+    """Fixture to delete the `tests/temp/` directory, before and after a test.
+
+    Yields:
+        Generator[None, None, None]: pytest fixture.
+    """
+    shutil.rmtree('tests/temp/', ignore_errors=True)
+    yield None
+    shutil.rmtree('tests/temp/', ignore_errors=True)
